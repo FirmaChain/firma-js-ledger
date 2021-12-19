e12266ac9aec0e909e39c8ad0ca6a8dc8ea4622c
fly33499 <fly33499@gmail.com>
2021-12-19 10:28:59 +0900

    feat: add ledger wallet for react-web

---
+++ sdk/FirmaLedgerWallet.ts
@@ -0,0 +1,133 @@
+import { Secp256k1Signature } from "./secp256k1signature";
+
+import CosmosApp from ".";
+import { LedgerWalletInterface } from "..";
+
+export class FirmaWebLedgerWallet implements LedgerWalletInterface {
+
+  public constructor(transportHID: any) {
+    this.transportHID = transportHID;
+  }
+
+  private transportHID: any;
+  private cosmosApp: CosmosApp | undefined;
+  private path: number[] = [44, 7777777, 0, 0, 0];
+
+  async showAddressOnDevice() {
+
+    try {
+
+      await this.connectLedger();
+
+      const response = await this.cosmosApp!.showAddressAndPubKey(this.path, "firma");
+      console.log("[Ledger]" + response);
+
+      this.closeLedger();
+
+    } catch (error) {
+      this.closeLedger();
+    }
+
+  }
+
+  async getPublicKey(): Promise<Uint8Array> {
+
+    try {
+
+      await this.connectLedger();
+
+      const response1 = await this.cosmosApp!.publicKey(this.path);
+
+      this.closeLedger();
+
+      if (response1.return_code !== 0x9000) {
+        throw new Error(`Error [${response1.return_code}] ${response1.error_message}`);
+      }
+
+      return response1.compressed_pk;
+    } catch (error) {
+      this.closeLedger();
+      throw error;
+    }
+  }
+
+  public async getAddress(): Promise<string> {
+
+    try {
+      await this.connectLedger();
+
+      const response = await this.cosmosApp!.getAddressAndPubKey(this.path, "firma");
+
+      this.closeLedger();
+
+      if (response.return_code !== 0x9000) {
+        console.log(`Error [${response.return_code}] ${response.error_message}`);
+        return "";
+      }
+
+      console.log("[Ledger]" + response);
+      return response.bech32_address;
+
+    } catch (error) {
+      this.closeLedger();
+      throw error;
+    }
+
+  }
+
+  private async closeLedger() {
+    try {
+      this.cosmosApp?.transport.close();
+      this.isOpen = false;
+    } catch (error) {
+      console.log("connect fail : " + error);
+      this.isOpen = false;
+      throw error;
+    }
+  }
+
+  private isOpen: boolean = false;
+
+  private async connectLedger() {
+    try {
+
+      if (this.isOpen)
+        return;
+
+      let transport = await this.transportHID.create();
+
+      if (transport != null) {
+        console.log("[Ledger] transport.deviceModel : " + transport.deviceModel!.id);
+        console.log("[Ledger] transport.deviceModel : " + transport.deviceModel!.productName);
+
+        this.cosmosApp = new CosmosApp(transport);
+
+        this.isOpen = true;
+
+        console.log("[Ledger] connect success");
+      }
+    } catch (error) {
+      console.log("connect fail : " + error);
+      this.isOpen = false;
+      throw error;
+    }
+  }
+
+  async sign(message: string): Promise<Uint8Array> {
+    try {
+
+      await this.connectLedger();
+
+      let response = await this.cosmosApp!.sign(this.path, message);
+      console.log(response);
+
+      let secp256k1 = Secp256k1Signature.fromDer(response.signature).toFixedLength();
+      this.closeLedger();
+
+      return Buffer.from(secp256k1);
+
+    } catch (error) {
+      throw error;
+    }
+  }
+}
